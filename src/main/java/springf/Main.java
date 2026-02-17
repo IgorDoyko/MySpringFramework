96256f32239c8de2cf93559950131fb1c4be14e7
Дойко Игорь <igor_do@mail.ru>
2026-02-17 22:53:27 +0300

    SpringFramework third lesson. Использование Dependency injection (внедрение зависимости через конфигурацию, а не через создание объекта)

--- src/main/java/springf/Main.java
+++ src/main/java/springf/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+
         context.close();
     }
 }
