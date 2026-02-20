71f204bc03fcc3670c8ece9157ecd8c0365baab3
Дойко Игорь <igor_do@mail.ru>
2026-02-20 21:27:39 +0300

    SpringFramework sixth lesson. Жизненный цикл бина. Init  и destroy методы. Как работают при singleton и prototype Фабричный метод. Приватный конструктор + доступ через публичный статичный метод

--- src/main/java/springf/ClassicalMusic.java
+++ src/main/java/springf/ClassicalMusic.java
@@ -2,8 +2,20 @@ package springf;
 
 public class ClassicalMusic implements Music{
 
+    private ClassicalMusic() {
+    }
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "Симфония № ";
+    }
+    public void init(){
+        System.out.println("инициализация");
+    }
+    public void destroy(){
+        System.out.println("финиш");
     }
 }
--- src/main/java/springf/Main.java
+++ src/main/java/springf/Main.java
@@ -7,19 +7,22 @@ public class Main {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
-        boolean equals = firstMusicPlayer == secondMusicPlayer;
-
-        System.out.println(equals);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(150);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean equals = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(equals);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(150);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
 
