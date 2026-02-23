d0ea953bf359ad592c2896ce8db14573dd2b3139
Дойко Игорь <igor_do@mail.ru>
2026-02-23 20:06:55 +0300

    SpringFramework ninth lesson. Использование @Qualifier. Указали 2 бина, какой для чего

--- src/main/java/springf/MusicPlayer.java
+++ src/main/java/springf/MusicPlayer.java
@@ -1,20 +1,23 @@
 package springf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+
+    private Music music;
+    private Music music1;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music,
+                       @Qualifier("rockMusic") Music music1) {
+        this.music = music;
+        this.music1 = music1;
     }
 
     public String playMusic() {
-        return "Play: " + classicalMusic.getSong();
+        return "Play: " + music.getSong() + " " + music1.getSong();
     }
 }
